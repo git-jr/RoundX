30d3988c141b20b0a5804e605d0452558a1bf641
git-jr <junior.carvalho@angellira.com.br>
2024-11-16 20:46:34 -0300

    feat: add Kamel to load image

--- composeApp/src/commonMain/kotlin/com/kmp/hango/App.kt
+++ composeApp/src/commonMain/kotlin/com/kmp/hango/App.kt
@@ -3,11 +3,16 @@ package com.kmp.hango
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -19,6 +24,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -33,6 +41,8 @@ import com.kmp.hango.navigation.Routes
 import com.kmp.hango.ui.InitScreen
 import com.kmp.hango.ui.categoryDetail.CategoryDetailScreen
 import com.kmp.hango.ui.game.GameScreen
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
 import kotlinx.serialization.ExperimentalSerializationApi
 
 @OptIn(ExperimentalCoilApi::class)
@@ -44,7 +54,27 @@ fun App() {
                 .crossfade(true)
                 .build()
         }
-        HomeScreen()
+
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            KamelImage(
+                resource = asyncPainterResource(data = "https://avatars.githubusercontent.com/u/35709152?v=4"),
+                contentDescription = "description",
+                modifier = Modifier
+                    .border(
+                        width = 4.dp,
+                        color = Color.White,
+                        shape = RoundedCornerShape(25.dp)
+                    )
+                    .size(150.dp)
+                    .clip(shape = RoundedCornerShape(25.dp)),
+            )
+        }
+
+
+//        HomeScreen()
     }
 }
 
